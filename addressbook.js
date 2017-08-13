2c85e4b2410ac6267ce393e5058ee776b5f861b7
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2017-08-13 13:57:20 +0100

    Add Kwadwo object

--- addressbook.js
+++ addressbook.js
@@ -4,3 +4,10 @@ var kwasi = {
     telephoneNo: "08084651232",
     email: "kwasi@email.com"
 };
+
+var kwadwo = {
+    firstName: "Kwadwo Amankwah",
+    lastName: "Asiedu",
+    telephoneNo: "08085551111",
+    email: "kwadwo@email.com"
+};
